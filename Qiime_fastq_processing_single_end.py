6eff41a58d714e7a90be2930a0c6319d80a32b0a
rostomides <larbi.bedrani@gmail.com>
2018-05-24 10:22:38 -0400

    comments updated

--- Qiime_fastq_processing_single_end.py
+++ Qiime_fastq_processing_single_end.py
@@ -6,7 +6,7 @@
 #Version: 1.6
 
 
-#This script allow to batch process single end fastq files (with no barcodes) using qiime 1.9. The script assume qiime 1.9 is installed in the linux system.
+#This script allows to batch process single-end fastq files (with no barcodes) using qiime 1.9. The script assumes qiime 1.9 is installed in the linux system. This scrpit assumes that all fastq files are stored in the same folder.
 
 import os
 import sys
@@ -15,20 +15,20 @@ raw_files = sys.argv[1]
 output_bioms = sys.argv[2]
 
 
-#Create 2 log file, one for processed fastqs and the second for the unprocessed fastqs
+#Create 2 log files, one for processed samples and the second for the unprocessed samples
 unprocessed = open("Unanalyzed_samples.txt", "w")
 processed = open("Analyzed_samples.txt", "w")
 
 
-
-os.chdir(raw_files) #collecting the list of files
+#Change to the folder containing the raw fastq files
+os.chdir(raw_files) 
 
 for i in os.listdir() and ".fasq" in i:
 	#Dimupltiplexing
 	com = ["split_libraries_fastq.py -i ", i , "--sample_ids", i.split(".fastq")[0], "-o slout/ -q 19 --barcode_type 'not-barcoded' --phred_offset 33"]
 	os.system(com); com=[]
 	
-	#Otu Picking		
+	#OTU Picking		
 	os.system("pick_closed_reference_otus.py -i $PWD/slout/seqs.fna -o $PWD/slout/otus_w_tax/ -f") 
 
 	#Checking if the otu table has been generated : otu_table.biom has been created?
@@ -41,7 +41,7 @@ for i in os.listdir() and ".fasq" in i:
 
 	if "otu_table.biom" in files:
 
-		#Moving the biom file to Uk_twins_bioms	
+		#Moving the biom file to output biom folder	
 		com = ["mv slout/otus_w_tax/otu_table.biom", output_bioms + "/" + i.split(".fastq")[0] + "biom"]
 		os.system(com); com=[]
 
@@ -53,10 +53,10 @@ for i in os.listdir() and ".fasq" in i:
 
 	else:
 		os.system("rm slout/otus_w_tax/ -r")
-		#Reverse complement the seqs.fna: this is necessary for some fastqs from old sequencing technologies
+		#Reverse complement the seqs.fna: this is necessary in some cases
 		os.system("adjust_seq_orientation.py -i slout/seqs.fna -o slout/rc_seqs.fna")
 							
-		#Otu Picking
+		#OTU Picking
 		os.system("pick_closed_reference_otus.py -i $PWD/slout/rc_seqs.fna -o $PWD/slout/otus_w_tax/ -f") 
 
 		try:
@@ -65,7 +65,7 @@ for i in os.listdir() and ".fasq" in i:
 			files=[]
 			
 		if "otu_table.biom" in files:
-			#Moving the biom file to Uk_twins_bioms
+			#Moving the biom file to output biom folder
 			com = ["mv slout/otus_w_tax/otu_table.biom", output_bioms + "/" + i.split(".fastq")[0] + "biom"]
 			os.system(com); com=[]			
 
@@ -74,7 +74,7 @@ for i in os.listdir() and ".fasq" in i:
 			#Remove the intermediary folder created by qiime
 			os.system("rm slout -r")
 		else:
-			#The file could not be processed, Add it to log file
+			#The file could not be processed, Add its name to log file
 			unprocessed.write(i + "\n")
 			#Remove the intermediary folder created by qiime
 			os.system("rm slout -r")
